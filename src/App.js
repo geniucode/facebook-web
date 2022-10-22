08f943f83ff42550cee9d443b55e8039711c4191
Ahmed <ahmed_ad_07@hotmail.com>
2022-10-22 13:41:29 +0300

    create forget password page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Signup } from "./components/signup";
+import { ForgetPassword } from "./components/forgetPassword";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/user/signup" element={<Signup />} />
+        <Route path="/forgetPassword" element={<ForgetPassword />} />
       </Routes>
     </Router>
   );
---
+++ src/components/forgetPassword/index.js
@@ -0,0 +1,40 @@
+import "./style.css";
+import facebook from "./images/Facebook-Logo.png";
+
+const ForgetPassword = () => {
+  return (
+    <div className="forget-password">
+      <div className="header">
+        <div className="logo">
+          <img src={facebook} alt="Facebook" />
+        </div>
+        <div className="signin-inputs">
+          <input type="email" placeholder="Email or phone" onChange="" />
+          <input type="password" placeholder="Password" onChange="" />
+          <button onClick="">Log in</button>
+          <a href="/forgetPassword">Forgotten account?</a>
+        </div>
+      </div>
+      <div className="forget-password-container">
+        <div className="forget-password-form">
+          <div className="title">Find Your Account</div>
+          <div className="details">
+            Please enter your email address or mobile number
+          </div>
+          <div className="email-input">
+            <input
+              type="email"
+              placeholder="Email address or mobile number"
+              onChange=""
+            />
+          </div>
+          <div className="forget-button">
+            <button onClick="">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { ForgetPassword };
