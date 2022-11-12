a0842409e1a95857e9cb8aa5e71a97bd62ac1868
anas <eng.anas.anbtawi@gmail.com>
2022-11-12 14:29:11 +0200

    FAC-30-Web

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Login } from "./components/login";
-import { Signup } from "./components/signup";
+import { Signup } from "./screen/signup";
 import { ForgetPassword } from "./components/forgetPassword";
 import { ResetPassword } from "./screen/resetPassword";
 function App() {
