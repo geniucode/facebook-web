f357bacb5f1db47d8b9200e655b8a3ab481cc49a
Omar-Odeh <omarodeh.2001@hotmail.com>
2022-11-29 04:51:46 +0200

    home page header

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,19 +1,18 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useSetRecoilState, useRecoilValue } from "recoil";
 import { userState } from "../../atoms/user";
 import { postAxios } from "../../src/service/axios";
+import HomeHeader from "../../components/homeHeader";
 
 const Home = () => {
   const router = useRouter();
   const setUser = useSetRecoilState(userState);
   const getData = async () => {
     if (localStorage.getItem("token")) {
-      const response = await postAxios(
-        "validate-token",
-        { jwtToken: localStorage.getItem("token") }
-      );
+      const response = await postAxios("validate-token", {
+        jwtToken: localStorage.getItem("token"),
+      });
       console.log("response", response);
       if (response.success) {
         setUser(response.user);
@@ -28,7 +27,11 @@ const Home = () => {
     getData();
   }, []);
   const user = useRecoilValue(userState);
-  return <>Hey {user.email}</>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 };
 
 export default Home;
