77a4d1bbe00c02e5602466ef74b90787ff55d9fd
RQawariq <rabeeqawariq@gmail.com>
2022-12-03 15:07:33 +0200

    Search

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
-import { Auth } from "../../components/auth";
+import Image from "next/image";
 import { useSearch } from "./useSearch";
+import { Auth } from "../../components/auth";
 import styles from "../../styles/search.module.css";
+import profilePicture from "./images/profilePicture.jpeg";
 
 const Search = () => {
-  const { error, onChangeSearchValue, onClickSearch } = useSearch();
+  const { error, users, onChangeSearchValue, onClickSearch } = useSearch();
   return (
     <>
       <Auth>
@@ -21,6 +23,38 @@ const Search = () => {
             <input type="submit" onClick={onClickSearch} value="Search"></input>
           </div>
           {error && <p className={styles.errorMsg}>{error}</p>}
+
+          <div className={styles.searchResult}>
+            {users.length > 0 &&
+              users.map((user) => {
+                return (
+                  <div className={styles.userWidget}>
+                    <div className={styles.userDetails}>
+                      <div className={styles.userImg}>
+                        <Image
+                          className={styles.profilePicture}
+                          src={profilePicture}
+                          alt="Profile Picture"
+                        ></Image>
+                      </div>
+                      <div className={styles.userFullName}>{user.email}</div>
+                    </div>
+                    <div className={styles.userButtons}>
+                      <input
+                        type="button"
+                        className={styles.addFriend}
+                        value="Add Friend"
+                      ></input>
+                      <input
+                        type="button"
+                        className={styles.removeSuggestion}
+                        value="Remove"
+                      ></input>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </Auth>
     </>
