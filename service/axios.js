77c212a75a669c3bcbc8c4388ba6ea24a1fe7548
mohammed Faraj <function.faraj@gmail.com>
2022-12-10 19:15:14 +0300

    postWithImageAxios

--- service/axios.js
+++ service/axios.js
@@ -15,15 +15,39 @@ const postAxios = async (url, data) => {
     return error.response?.data;
   }
 };
-const getAxios = async (url, data) => {
+const postWithImageAxios = async (url, data) => {
+  const token = localStorage.getItem("token");
+  const headers = {
+    "Content-Type": "multipart/form-data; boundary=something",
+    Authorization: "Bereare " + token,
+  };
   try {
-    const response = await axios.get(`http://127.0.0.1:3001/${url}`, {
-      params: data,
+    const response = await axios.post(`http://127.0.0.1:3001/${url}`, data, {
+      headers,
     });
+    return response.data;
+  } catch (error) {
+    return error.response?.data;
+  }
+};
+const getAxios = async (url, data) => {
+  const token = localStorage.getItem("token");
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bereare " + token,
+  };
+  try {
+    const response = await axios.get(
+      `http://127.0.0.1:3001/${url}`,
+      {
+        params: data,
+      },
+      { headers }
+    );
     return response.data;
   } catch (error) {
     return error.response.data;
   }
 };
 
-export { postAxios, getAxios };
+export { postAxios, getAxios, postWithImageAxios };
