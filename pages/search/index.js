47b53152c8bb961589f4bf21aff1b182ae3bc424
RQawariq <rabeeqawariq@gmail.com>
2022-12-12 13:37:06 +0200

    Fix search css & display

--- pages/search/index.js
+++ pages/search/index.js
@@ -4,10 +4,10 @@ import Link from "next/link";
 import { useSearch } from "./useSearch";
 import { Auth } from "../../components/auth";
 import { HomePageMneu } from "../../components/homePageMenu";
-import facebookIcon from "./images/facebook-icon.png";
-import searchIcon from "./images/search-icon.png";
+import pfp from "./images/pfp.jpg";
 import profilePicture from "./images/profile-picture.jpeg";
 import styles from "../../styles/search.module.css";
+import { style } from "@mui/system";
 
 const Search = () => {
   const { error, users, onChangeSearchValue, onClickSearch } = useSearch();
@@ -17,61 +17,99 @@ const Search = () => {
         <Head>
           <title>Search</title>
         </Head>
-
-        <div className={styles.search}>
-          <div className={styles.searchHeader}>
-            <Link href="/home">
-              <Image src={facebookIcon} alt="Facebook Icon"></Image>
+        <div className={styles.headerContainer}>
+          <div className={styles.headerContent}>
+            <Link href={"/home"}>
+              <svg height="22" viewBox="100 100 822.51 160">
+                <title>Facebook</title>
+                <path d="M166.43,126.68c-9.65,0-12.44,4.28-12.44,13.72v15.66h25.74l-2.58,25.3H154v76.78H123.11V181.36H102.3v-25.3h20.81V140.83c0-25.52,10.29-39,39-39a146.17,146.17,0,0,1,18,1.07v23.81Z"></path>
+                <path d="M181.87,203.88c0-28.52,13.51-50,41.82-50,15.44,0,24.87,7.94,29.38,17.8V156.06h29.59V258.14H253.07V242.7c-4.29,9.87-13.94,17.59-29.38,17.59-28.31,0-41.82-21.45-41.82-50Zm30.88,6.87c0,15.22,5.57,25.3,19.94,25.3,12.66,0,19.09-9.22,19.09-23.8V202c0-14.58-6.43-23.8-19.09-23.8-14.37,0-19.94,10.08-19.94,25.3Z"></path>
+                <path d="M347,153.91c12,0,23.37,2.58,29.59,6.86l-6.86,21.88a48.6,48.6,0,0,0-20.59-4.72c-16.73,0-24,9.65-24,26.17v6c0,16.52,7.29,26.17,24,26.17a48.6,48.6,0,0,0,20.59-4.72l6.86,21.87c-6.22,4.29-17.58,6.87-29.59,6.87-36.25,0-52.76-19.52-52.76-50.83v-4.72C294.24,173.43,310.75,153.91,347,153.91Z"></path>
+                <path d="M380.66,211v-9c0-28.95,16.51-48,50.19-48,31.74,0,45.68,19.3,45.68,47.61v16.3h-65c.65,13.94,6.87,20.16,24,20.16,11.59,0,23.81-2.36,32.82-6.22L474,253c-8.15,4.3-24.88,7.51-39.67,7.51C395.24,260.5,380.66,241,380.66,211Zm30.88-13.3h37.32v-2.57c0-11.15-4.5-20-18-20C416.91,175.14,411.54,183.94,411.54,197.66Z"></path>
+                <path d="M591,210.32c0,28.52-13.72,50-42,50-15.44,0-26.16-7.72-30.45-17.59v15.44H489.39V104.8L520.27,102v68.2c4.5-9,14.37-16.3,28.74-16.3,28.31,0,42,21.45,42,50Zm-30.88-7.08c0-14.37-5.57-25.09-20.37-25.09-12.66,0-19.52,9-19.52,23.59v10.72c0,14.58,6.86,23.59,19.52,23.59,14.8,0,20.37-10.72,20.37-25.09Z"></path>
+                <path d="M601.33,209.67v-5.14c0-29.39,16.73-50.62,50.83-50.62S703,175.14,703,204.53v5.14c0,29.38-16.73,50.62-50.83,50.62S601.33,239.05,601.33,209.67Zm70.78-7.29c0-13.51-5.58-24.23-20-24.23s-19.95,10.72-19.95,24.23v9.44c0,13.51,5.58,24.23,19.95,24.23s20-10.72,20-24.23Z"></path>
+                <path d="M713.27,209.67v-5.14c0-29.39,16.73-50.62,50.83-50.62s50.83,21.23,50.83,50.62v5.14c0,29.38-16.73,50.62-50.83,50.62S713.27,239.05,713.27,209.67Zm70.78-7.29c0-13.51-5.58-24.23-19.95-24.23s-19.94,10.72-19.94,24.23v9.44c0,13.51,5.57,24.23,19.94,24.23s19.95-10.72,19.95-24.23Z"></path>
+                <path d="M857.39,204.74l30.45-48.68h32.81l-31.95,50.4,33.24,51.68H889.13l-31.74-50v50H826.5V104.8L857.39,102Z"></path>
+              </svg>
             </Link>
-            <div className={styles.searchBar}>
+            <div className={styles.inputContainer}>
+              <div className={styles.searchIcon}>
+                <svg
+                  onClick={onClickSearch}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
+                </svg>
+              </div>
               <input
-                type="text"
-                placeholder="Search Facebook"
                 onChange={onChangeSearchValue}
+                className={styles.searchInput}
+                placeholder="Search Facebook"
               ></input>
-              <button type="submit" onClick={onClickSearch}>
-                <Image src={searchIcon} alt="Search"></Image>
-              </button>
-              {error && <p className={styles.errorMsg}>{error}</p>}
             </div>
-          </div>
-          <div className={styles.bodyContainer}>
-            <div>
-              <HomePageMneu />
-            </div>
-            <div>
-              <div className={styles.searchResult}>
-                {users.length > 0 &&
-                  users.map((user) => {
-                    return (
-                      <div className={styles.userWidget}>
-                        <div className={styles.userDetails}>
-                          <div className={styles.userImg}>
-                            <Image
-                              className={styles.profilePicture}
-                              src={profilePicture}
-                              alt="Profile Picture"
-                            ></Image>
-                          </div>
-                          <div className={styles.userFullName}>{user.name}</div>
-                        </div>
-                        <div className={styles.userButtons}>
-                          <input
-                            type="button"
-                            className={styles.addFriend}
-                            value="Add Friend"
-                          ></input>
-                          <input
-                            type="button"
-                            className={styles.removeSuggestion}
-                            value="Remove"
-                          ></input>
-                        </div>
-                      </div>
-                    );
-                  })}
+            <div className={styles.haederIconsContainer}>
+              <div className={styles.haederIcon}>
+                <svg viewBox="0 0 28 28" height="20" width="20">
+                  <path d="M14 2.042c6.76 0 12 4.952 12 11.64S20.76 25.322 14 25.322a13.091 13.091 0 0 1-3.474-.461.956 .956 0 0 0-.641.047L7.5 25.959a.961.961 0 0 1-1.348-.849l-.065-2.134a.957.957 0 0 0-.322-.684A11.389 11.389 0 0 1 2 13.682C2 6.994 7.24 2.042 14 2.042ZM6.794 17.086a.57.57 0 0 0 .827.758l3.786-2.874a.722.722 0 0 1 .868 0l2.8 2.1a1.8 1.8 0 0 0 2.6-.481l3.525-5.592a.57.57 0 0 0-.827-.758l-3.786 2.874a.722.722 0 0 1-.868 0l-2.8-2.1a1.8 1.8 0 0 0-2.6.481Z"></path>
+                </svg>
+              </div>
+              <div className={styles.haederIcon} notificationcount={`+9`}>
+                <svg viewBox="0 0 28 28" height="20" width="20">
+                  <path d="M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z"></path>
+                </svg>
+              </div>
+              <div
+                className={`${styles.haederIcon} ${styles.headerProfilePic}`}
+              >
+                <Image src={pfp} alt="pfp" />
+                <div className={styles.chevronDown}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="512"
+                    height="298"
+                    viewBox="0 0 512 298.04"
+                  >
+                    <path d="M12.08 70.78c-16.17-16.24-16.09-42.54.15-58.7 16.25-16.17 42.54-16.09 58.71.15L256 197.76 441.06 12.23c16.17-16.24 42.46-16.32 58.71-.15 16.24 16.16 16.32 42.46.15 58.7L285.27 285.96c-16.24 16.17-42.54 16.09-58.7-.15L12.08 70.78z" />
+                  </svg>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div className={styles.bodyContainer}>
+          <HomePageMneu />
+          <div className={styles.searchResult}>
+            {error && <p className={styles.errorMsg}>{error}</p>}
+            {users.length > 0 &&
+              users.map((user) => {
+                return (
+                  <div className={styles.userWidget}>
+                    <div className={styles.userDetails}>
+                      <div className={styles.userImg}>
+                        <Image
+                          className={styles.profilePicture}
+                          src={profilePicture}
+                          alt="Profile Picture"
+                        ></Image>
+                      </div>
+                      <div className={styles.userFullName}>{user.name}</div>
+                    </div>
+                    <div className={styles.userButtons}>
+                      <input
+                        type="button"
+                        className={styles.addFriend}
+                        value="Add Friend"
+                      ></input>
+                      <input
+                        type="button"
+                        className={styles.removeSuggestion}
+                        value="Remove"
+                      ></input>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </Auth>
