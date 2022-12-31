012e50e586c878790af6cabc8e936c880c1c08cd
RQawariq <rabeeqawariq@gmail.com>
2022-12-31 15:27:17 +0200

    fix

--- components/homeHeader/index.js
+++ components/homeHeader/index.js
@@ -5,19 +5,28 @@ import { useRecoilState } from "recoil";
 import { userState } from "../../atoms/user";
 import { searchUsersState } from "../../atoms/users";
 import { searchErrorState } from "../../atoms/error";
-import { getAxios } from "../../service/axios";
+import { getAxios, postAxios } from "../../service/axios";
 import styles from "../../styles/homeHeader.module.css";
 import pfp from "./images/pfp.jpg";
 import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 
 const HomeHeader = ({}) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
+  const onClickSetNotificationsTrue = () => {
+    const setNotificationsTrue = postAxios("updateNotifications", {
+      notifications,
+    });
+  };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
@@ -114,24 +123,49 @@ const HomeHeader = ({}) => {
             className={styles.haederIcon}
             notificationcount={notifications?.length}
           >
-            <svg viewBox="0 0 28 28" height="20" width="20">
-              <path d="M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z">
-                {notifications &&
-                  notifications?.map((notification) => {
-                    // notification.notification = "true";
-                    //eslint rules
-                    return (
-                      <>
-                        {/* <MenuItem>
-                            <Avatar />
-                            {notification.requester.name} has sent you a friend
-                            request!
-                          </MenuItem>
-                          <Divider /> */}
-                      </>
-                    );
-                  })}
-                {/* {notifications ? (
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={(event) => {
+                handleClick(event);
+                onClickSetNotificationsTrue();
+              }}
+            >
+              <svg viewBox="0 0 28 28" height="20" width="20">
+                <path d="M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z"></path>
+              </svg>
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              {notifications &&
+                notifications?.map((notification) => {
+                  // notification.notification = "true";
+                  //eslint rules
+                  return (
+                    <>
+                      <MenuItem>
+                        <Avatar />
+                        <div style={{ marginLeft: "5px" }}>
+                          {notification.requester.name} has sent you a friend
+                          request!
+                        </div>
+                      </MenuItem>
+                      <Divider />
+                    </>
+                  );
+                })}
+            </Menu>
+
+            {/* {notifications ? (
                     notifications.map((notification) => {
                       <>
                         <MenuItem>
@@ -145,8 +179,6 @@ const HomeHeader = ({}) => {
                   ) : (
                     <MenuItem>You have no notifications</MenuItem>
                   )} */}
-              </path>
-            </svg>
           </div>
           <div className={`${styles.haederIcon} ${styles.headerProfilePic}`}>
             <Image src={pfp} alt="pfp" />
