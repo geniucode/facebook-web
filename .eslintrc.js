380e55149a0f959810f38433611c34bb4cd85407
Anas Anbtawi <eng.anas.anbtawi@gmail.com>
2023-01-07 13:03:48 +0200

    eslint-rules-modified2

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,20 +13,20 @@ module.exports = {
   rules: {
     // don't allow console logs (Faraj asked for it)
     "no-console": "on",
-
-    indent: ["error", 2], //this only allow up to 2 spaces
+    "indent": ["error", 2], //this only allow up to 2 spaces
     "no-var": ["error", 2], //this doesn't allow using Var only Let or constant are allowed
+    "comma-style": ["error", "last"], //adding comma is always direct after sentence and not on the start
+    "max-len": [
+      //line maximum lenght rules
+      "error",
+      {
+        code: 60,
+        tabWidth: 2,
+        ignoreComments: true, //"comments": 80
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
   },
-
-  "max-len": [  //line maximum lenght ruels 
-    "error",
-    {
-      code: 60,
-      tabWidth: 2,
-      ignoreComments: true, //"comments": 80
-      ignoreUrls: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    },
-  ],
 };
